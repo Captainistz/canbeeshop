b123cc4ef08b9d4ade37f192b64d5ceb9e82dab2
Captainistz <mynameisjeff7614@gmail.com>
2022-04-26 14:41:34 +0700

    🚀 change next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }
 
     return config
-  }
+  },
 }
 
 module.exports = nextConfig
